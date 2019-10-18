1c4af6bc7951f2a60831227ddb382628bca7c205
Gabriel Seals <sealsgj@gmail.com>
2019-10-17 21:29:36 -0500

    transitions to other parts of the site and resize jumping to top

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,13 @@
 import '../styles/main.scss';
 import './components/navbar/navbar.scss';
 import navbar from './components/navbar/navbar';
-import dark from './components/dark-light';
+import dark from './components/darkTheme/dark-light';
+import size from './components/fontSize/fontSize';
 
 const init = () => {
   navbar.formEnter();
   dark.event();
+  size.sizer();
 };
 
 init();
