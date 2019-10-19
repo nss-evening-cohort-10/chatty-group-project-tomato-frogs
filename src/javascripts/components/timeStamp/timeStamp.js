a294fbf1a5e40e4880f253bb3fecc9518f9f00b0
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-10-19 10:07:23 -0500

    wip

--- src/javascripts/components/timeStamp/timeStamp.js
+++ src/javascripts/components/timeStamp/timeStamp.js
@@ -1,9 +1,8 @@
 import $ from 'jquery';
 import moment from 'moment';
-import m from '../../helpers/data/messages';
+
 
 const addTimeStamp = () => {
-  const messages = m.getMessages();
   const TimeStamp = moment().format();
   let domString = '';
   domString += `${TimeStamp}`;
