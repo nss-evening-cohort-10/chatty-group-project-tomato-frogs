b363d8946f6ee9697e66f102a1483c11f4c7d043
Gabriel Seals <sealsgj@gmail.com>
2019-10-19 10:00:59 -0500

    adding text sizer

--- src/javascripts/components/fontSize/fontSize.js
+++ src/javascripts/components/fontSize/fontSize.js
@@ -2,10 +2,17 @@ import $ from 'jquery';
 
 const sizer = () => {
   $('#textSize').on('click', () => {
-    for (let i = 10; i < 50; i += 1) {
-      $('#bodySize').css('font-size', `${[i]}px`);
-    }
+    let fontSize = parseInt($('body').css('font-size'), 0);
+    fontSize = `${fontSize + 1}px`;
+    $('body').css({ 'font-size': fontSize });
   });
 };
+
+//   for (let i = 10; i < 50; i += 1) {
+//     if (i < 12) {
+//       $('#bodySize').css('font-size', `${[i]}px`);
+//     }
+//   }
+// });
 
 export default { sizer };
