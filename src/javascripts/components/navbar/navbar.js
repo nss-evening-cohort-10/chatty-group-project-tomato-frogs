537523d2462d8ddc0fdf54f5614d27c1eb56f4d6
Kelsey Creel <kelseycreel@gmail.com>
2019-10-21 18:17:11 -0500

    styling

--- src/javascripts/components/navbar/navbar.js
+++ src/javascripts/components/navbar/navbar.js
@@ -12,11 +12,9 @@ const addMessage = (event) => {
       newMsgObj.messageText = $('#text-area-id').val();
       newMsgObj.timeStamp = 'timestamp';
       event.preventDefault();
-      console.log(newMsgObj);
       const newMsgArr = messages.getMessages();
       newMsgArr.unshift(newMsgObj);
       display.displayMessages(newMsgArr);
-      console.log(newMsgArr);
     }
   }
 };
